5de2cb55df45a3f71be22d528e6ba7f4b576fede
James Provance <jb24x7@gmail.com>
2023-03-13 05:45:16 -0700

    set up order routing and pages

---
+++ VendorOrder/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrder.Models;
+
+
+namespace VendorOrder.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+    [HttpGet("/vendor/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendor/{vendorId}/orders/{ordersId}")]
+    public ActionResult Show(int vendorId, int ordersId)
+    {
+      Orders order = Orders.Find(ordersId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("orders", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+  }
+}
